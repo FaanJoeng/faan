afed702e4f4415f0591839d49a993c760372ffd3
FaanJoeng <1135611487@qq.com>
2017-02-21 21:59:59 +0800

    modify

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello, Faan!');
-});
+//common setup
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//log print setup
+app.use(logger('dev'));
+
+//router setup 
+var routes = require('./routes/index');
+var register = require('./routes/register');
+app.use('/', routes);
+app.use('/register', register);
+
+//view engine setup 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+
+//static resource setup
+app.use(express.static('public'));
+
 
 var server = app.listen(80, function () {
   var host = server.address().address;
@@ -11,3 +35,22 @@ var server = app.listen(80, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    title: 'Wrong!',
+    error: err
+  });
+});
+
+
+module.exports = app;
