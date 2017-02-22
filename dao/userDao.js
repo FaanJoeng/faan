58d141c77ba2be16e292f6ed4d1fc302dd3e77e5
FaanJoeng <1135611487@qq.com>
2017-02-22 23:13:00 +0800

    modified

--- dao/userDao.js
+++ dao/userDao.js
@@ -1,22 +1,36 @@
 var mysql = require('mysql');
 var $conf = require('../conf/db');
 var $sql = require('./userSqlMapping');
+var $util = require('../utils/util');
 
-var pool = mysql.createPool($conf.mysql);
+var pool = mysql.createPool($util.extend({}, $conf.mysql));
 
 module.exports = {
-	add: function (req, res, hash, salt) {
+	add: function(req, res, hash, salt){
 		var username = req.body.username;
-        pool.getConnection(function(err, connection) {
-        	connection.query($sql.add, username, hash, salt, function(err, result){
+        pool.getConnection(function(err, connection){
+        	connection.query($sql.add, [username, hash, salt], function(err, result){
         		if(err){
-        			console.log(err)
+        			console.log(err);
         		}
         		if(result){
-        			res.send('success!');
+        			res.status(200).end('success!');
         		}
         	});
         	connection.release();
         });
-	}
+	},
+
+    query: function(req, res, fn){
+        var username = req.body.username;
+        pool.getConnection(function(err, connection){
+            connection.query($sql.query, [username], function(err, result){
+                if(err)
+                    console.log(err);
+                if(result){
+                    fn(req, res, result);
+                }
+            });
+        });
+    }
 }
--- dao/userSqlMapping.js
+++ dao/userSqlMapping.js
@@ -1,6 +1,7 @@
 var sql = {
 	add: 'INSERT INTO user(id, username, hash, salt) VALUES (0, ?, ?, ?)',
-	isExisted: 'SELECT username from user WHERE username = ?'
+	isExisted: 'SELECT username from user WHERE username = ?',
+	query: 'SELECT * FROM user WHERE username = ?'
 }
 
 module.exports = sql;
