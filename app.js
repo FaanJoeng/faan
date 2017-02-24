562661945ab65409cec77a6416e1bb08631ca50d
FaanJoeng <1135611487@qq.com>
2017-02-24 12:36:15 +0800

    modified

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
 
 
